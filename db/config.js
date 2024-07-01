ee531b1e11a998ff1a7e7236bc7e79c4a5fb68aa
Deepak Prajapat <94034668+deepak411prajapat@users.noreply.github.com>
2024-07-01 18:08:21 +0530

    config.js

--- db/config.js
+++ db/config.js
@@ -1,2 +1,2 @@
 const mongoose= require('mongoose');
-mongoose.connect("mongodb://0.0.0.0:27017/e-comm");
+mongoose.connect("mongodb+srv://deepakprajapat160499:zNmkMr45cXwtJEth@kidsclasses.chhjbl3.mongodb.net/k-class?retryWrites=true&w=majority&appName=kidsclasses");
