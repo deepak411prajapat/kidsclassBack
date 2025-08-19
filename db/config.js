d237c138ba12b797ef3a361dc51bdc2807e794a0
Deepak Prajapat <94034668+deepak411prajapat@users.noreply.github.com>
2025-08-19 11:36:19 +0530

    config.js

--- db/config.js
+++ db/config.js
@@ -1,2 +1,11 @@
-const mongoose= require('mongoose');
-mongoose.connect("mongodb+srv://deepakprajapat160499:zNmkMr45cXwtJEth@kidsclasses.chhjbl3.mongodb.net/k-class?retryWrites=true&w=majority&appName=kidsclasses");
+//const mongoose= require('mongoose');
+//mongoose.connect("mongodb+srv://deepakprajapat160499:zNmkMr45cXwtJEth@kidsclasses.chhjbl3.mongodb.net/k-class?retryWrites=true&w=majority&appName=kidsclasses");
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected successfully!"))
+.catch((err) => console.error("MongoDB connection error:", err));
